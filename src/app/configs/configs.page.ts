df0c2ae4a8c52f3f89b4ac68d0fdf0e01e71607c
felipebiel <54965084+felipebiel@users.noreply.github.com>
2019-11-23 11:04:23 -0300

    AI-BETA-0.4.0

--- src/app/configs/configs.page.ts
+++ src/app/configs/configs.page.ts
@@ -81,6 +81,8 @@ export class ConfigsPage {
     this.deviceService.updateDevice(this.id, form).subscribe(
       data => {
         this.presentToast();
+        //alert(this.id);
+        this.deviceService.commandDevice(this.id, 'N'+this.nivel).subscribe()
       },
       error => {            
       }
